840cdb68b92688494ae7c730e174f8290f764466
Marcus Westin <marcus.westin@gmail.com>
2013-06-27 17:42:08 -0700

    Add FunAll.h for easy-sweasy import

---
+++ FunAll.h
@@ -0,0 +1,28 @@
+//
+//  FunBase.h
+//  Dogo-iOS
+//
+//  Created by Marcus Westin on 6/25/13.
+//  Copyright (c) 2013 Flutterby Labs Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "FunBase.h"
+
+#import "NSArray+Fun.h"
+#import "NSData+Fun.h"
+#import "NSDictionary+Fun.h"
+#import "NSObject+Fun.h"
+#import "NSString+Fun.h"
+#import "UIColor+Fun.h"
+#import "UIView+FunStyle.h"
+
+#import "API.h"
+#import "Cache.h"
+#import "Events.h"
+#import "Files.h"
+#import "Images.h"
+#import "JSON.h"
+#import "Log.h"
+#import "SQL.h"
