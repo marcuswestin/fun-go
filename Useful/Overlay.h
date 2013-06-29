3a7ee3125f6861079944f11e44f8c8136ad8107a
Marcus Westin <marcus.westin@gmail.com>
2013-06-28 18:24:36 -0700

    Add useful Overlay, but it is broken and currently disabled. Have to figure out how to properly deal with mutliple UIWIndows

---
+++ Useful/Overlay.h
@@ -0,0 +1,16 @@
+//
+//  Overlay.h
+//  Dogo-iOS
+//
+//  Created by Marcus Westin on 6/28/13.
+//  Copyright (c) 2013 Flutterby Labs Inc. All rights reserved.
+//
+
+#import "FunBase.h"
+
+@interface Overlay : FunBase
+
++ (void)show:(NSString*)message;
++ (void)hide;
+
+@end
---
+++ Useful/Overlay.m
@@ -0,0 +1,47 @@
+//
+//  Overlay.m
+//  Dogo-iOS
+//
+//  Created by Marcus Westin on 6/28/13.
+//  Copyright (c) 2013 Flutterby Labs Inc. All rights reserved.
+//
+
+#import "Overlay.h"
+#import "FunAll.h"
+
+@implementation Overlay
+
+static UIWindow* overlayWindow;
+
++ (void)show:(NSString *)message {
+    return;
+    [Overlay hide];
+    CGRect frame = [[UIScreen mainScreen] bounds];
+
+    UILabel* label = [[UILabel alloc] init];
+    label.text = message;
+    label.textColor = UIColor.whiteColor;
+
+    overlayWindow = [[UIWindow alloc] initWithFrame:frame];
+    overlayWindow.windowLevel = UIWindowLevelStatusBar + 1;
+    overlayWindow.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.75];
+    [overlayWindow addSubview:label.style.sizeToFit.centerIn(overlayWindow).apply];
+    [overlayWindow setHidden:NO];
+    
+    [self _hideKeyboard];
+}
+
++ (void)hide {
+    return;
+    if (!overlayWindow) { return; }
+    [overlayWindow setHidden:YES];
+    overlayWindow = nil;
+}
+
++ (void)_hideKeyboard {
+    UITextField* input = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
+    [input becomeFirstResponder];
+    [input resignFirstResponder];
+}
+
+@end
