f9a963ea7a24050a43736848f0e9a4427c789bf7
Marcus Westin <marcus.westin@gmail.com>
2013-07-09 16:10:32 -0700

    Add makeView(); Add UIView Category moveByX:y:, moveToX:y:, outsetShadowColor:opacity:radius:x:y, insetShadowColor:radius:x:y, borderColor:width

--- Categories/UIView+FunStyle.h
+++ Categories/UIView+FunStyle.h
@@ -10,6 +10,8 @@
 
 @class ViewStyle;
 
+ViewStyle* makeView();
+
 typedef ViewStyle* (^Styler)();
 typedef ViewStyle* (^StylerView)(UIView* view);
 typedef ViewStyle* (^StylerSize)(CGSize size);
@@ -39,15 +41,22 @@ typedef ViewStyle* (^StylerColor1)(UIColor* color);
 - (StylerSize)size;
 - (StylerView)centerIn;
 
-/* Color
- *******/
+/* Misc
+ ******/
 - (StylerColor1)bg;
-
+typedef ViewStyle* (^StylerRadius)(CGFloat radius);
+- (StylerRadius)radius;
 @end
 
 @interface UIView (FunStyle)
 
 - (ViewStyle*) style;
-- (void)moveX:(NSInteger)x y:(NSInteger)y;
+- (void)moveByX:(NSInteger)x y:(NSInteger)y;
+- (void)moveToX:(CGFloat)x y:(CGFloat)y;
+- (void)moveToX:(CGFloat)x;
+- (void)moveToY:(CGFloat)y;
+- (void)outsetShadowColor:(UIColor*)color opacity:(CGFloat)opacity radius:(CGFloat)radius x:(CGFloat)offsetX y:(CGFloat)offsetY;
+- (void)insetShadowColor:(UIColor*)fillColor radius:(CGFloat)radius x:(CGFloat)offsetX y:(CGFloat)offsetY;
+- (void)borderColor:(UIColor*)color width:(CGFloat)width;
 
 @end
--- Categories/UIView+FunStyle.m
+++ Categories/UIView+FunStyle.m
@@ -7,6 +7,11 @@
 //
 
 #import "UIView+FunStyle.h"
+#import <QuartzCore/QuartzCore.h>
+
+ViewStyle* makeView() {
+    return [[UIView alloc] init].style;
+}
 
 @implementation UIView (FunStyle)
 
@@ -14,36 +19,87 @@
     return [[ViewStyle alloc] initWithView:self];
 }
 
-- (void)moveX:(NSInteger)dx y:(NSInteger)dy {
+- (void)moveByX:(NSInteger)dx y:(NSInteger)dy {
     CGFloat x = self.frame.origin.x + dx;
     self.frame = CGRectMake(x, self.frame.origin.y + dy, self.frame.size.width, self.frame.size.height);
 }
 
+- (void)moveToX:(CGFloat)x y:(CGFloat)y {
+    self.frame = CGRectMake(x, y, self.frame.size.width, self.frame.size.height);
+}
+- (void)moveToY:(CGFloat)y {
+    [self moveToX:self.frame.origin.x y:y];
+}
+- (void)moveToX:(CGFloat)x {
+    [self moveToX:x y:self.frame.origin.y];
+}
+
+- (void)outsetShadowColor:(UIColor *)color opacity:(CGFloat)opacity radius:(CGFloat)radius x:(CGFloat)offsetX y:(CGFloat)offsetY {
+    self.layer.shadowColor = color.CGColor;
+    self.layer.shadowOpacity = opacity;
+    self.layer.shadowRadius = radius;
+    self.layer.shadowOffset = CGSizeMake(offsetX, offsetY);
+}
+
+- (void)insetShadowColor:(UIColor*)color radius:(CGFloat)radius x:(CGFloat)offsetX y:(CGFloat)offsetY {
+    UIView *shadowView = [[UIView alloc] initWithFrame:self.frame];
+    [shadowView moveToX:0 y:0];
+    NSArray* colors = @[(id)color.CGColor, (id)[UIColor.clearColor CGColor]];
+    
+    CAGradientLayer *shadowTop = [CAGradientLayer layer];
+    shadowTop.frame = CGRectMake(0 + offsetX, 0 + offsetY, self.bounds.size.width, radius);
+    shadowTop.colors = colors;
+    shadowTop.startPoint = CGPointMake(0.5, 0.0);
+    shadowTop.endPoint = CGPointMake(0.5, 1.0);
+    [shadowView.layer insertSublayer:shadowTop atIndex:0];
+    
+    CAGradientLayer *shadowRight = [CAGradientLayer layer];
+    shadowRight.frame = CGRectMake(self.bounds.size.width - radius + offsetX, 0 + offsetY, radius, self.bounds.size.height);
+    shadowRight.colors = colors;
+    shadowRight.startPoint = CGPointMake(0.5, 0.0);
+    shadowRight.endPoint = CGPointMake(0.5, 1.0);
+    [shadowView.layer insertSublayer:shadowRight atIndex:0];
+
+    CAGradientLayer *shadowBottom = [CAGradientLayer layer];
+    shadowBottom.frame = CGRectMake(0 + offsetX, self.bounds.size.height - radius + offsetY, self.bounds.size.width, radius);
+    shadowBottom.colors = colors;
+    shadowBottom.startPoint = CGPointMake(0.5, 1.0);
+    shadowBottom.endPoint = CGPointMake(0.5, 0.0);
+    [shadowView.layer insertSublayer:shadowBottom atIndex:0];
+
+    CAGradientLayer *shadowLeft = [CAGradientLayer layer];
+    shadowLeft.frame = CGRectMake(0 + offsetX, 0 + offsetY, radius, self.bounds.size.height);
+    shadowLeft.colors = colors;
+    shadowLeft.startPoint = CGPointMake(0.0, 0.5);
+    shadowLeft.endPoint = CGPointMake(1.0, 0.5);
+    [shadowView.layer insertSublayer:shadowLeft atIndex:0];
+    
+    [self addSubview:shadowView];
+}
+
+- (void)borderColor:(UIColor *)color width:(CGFloat)width {
+    self.layer.borderColor = color.CGColor;
+    self.layer.borderWidth = width;
+}
 @end
 
 @implementation ViewStyle {
-    UIView* view;
-    CGRect frame;
-    CALayer* layer;
+    UIView* _view;
+    CGRect _frame;
 }
 
 /* Create & apply
  ****************/
-- (instancetype)init {
-    self = [super init];
-    frame = CGRectZero;
-    return self;
-}
-
-- (instancetype)initWithView:(UIView *)_view {
+- (instancetype)initWithView:(UIView *)view {
     self = [self init];
-    view = _view;
+    _view = view;
+    _frame = view.frame;
     return self;
 }
 
 - (UIView *)apply {
-    view.frame = frame;
-    return view;
+    _view.frame = _frame;
+    return _view;
 }
 
 
@@ -51,31 +107,31 @@
  **********/
 - (StylerFloat1)x {
     return ^(float x) {
-        self.xy(x, view.frame.origin.y);
+        self.xy(x, _view.frame.origin.y);
         return self;
     };
 }
 
 - (StylerFloat1)y {
     return ^(float y) {
-        self.xy(view.frame.origin.x, y);
+        self.xy(_view.frame.origin.x, y);
         return self;
     };
 }
 
 - (StylerFloat2)xy {
     return ^(float x, float y) {
-        frame.origin.x = x;
-        frame.origin.y = y;
+        _frame.origin.x = x;
+        _frame.origin.y = y;
         return self;
     };
 }
 
 - (StylerView)centerIn {
     return ^(UIView* parentView) {
-        view.frame = frame;
-        view.center = parentView.center;
-        frame = view.frame;
+        _view.frame = _frame;
+        _view.center = parentView.center;
+        _frame = _view.frame;
         return self;
     };
 }
@@ -84,46 +140,52 @@
  ******/
 - (StylerFloat1)w {
     return ^(float width) {
-        frame.size.width = width;
+        _frame.size.width = width;
         return self;
     };
 }
 
 - (StylerFloat1)h {
     return ^(float height) {
-        frame.size.height = height;
+        _frame.size.height = height;
         return self;
     };
 }
 
 - (StylerFloat2)wh {
     return ^(float width, float height) {
-        frame.size.width = width;
-        frame.size.height = height;
+        _frame.size.width = width;
+        _frame.size.height = height;
         return self;
     };
 }
 
 - (StylerSize)size {
     return ^(CGSize size) {
-        frame.size = size;
+        _frame.size = size;
         return self;
     };
 }
 
 - (ViewStyle*)sizeToFit {
-    [view sizeToFit];
-    frame.size = view.frame.size;
+    [_view sizeToFit];
+    _frame.size = _view.frame.size;
     return self;
 }
 
-/* Color
- *******/
+/* Misc
+ ******/
 - (StylerColor1)bg {
     return ^(UIColor* color) {
-        view.backgroundColor = color;
+        _view.backgroundColor = color;
         return self;
     };
 }
 
+- (StylerRadius)radius {
+    return ^(CGFloat radius) {
+        _view.layer.cornerRadius = radius;
+        return self;
+    };
+}
 @end
