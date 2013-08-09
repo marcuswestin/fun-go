adffef2b81c29aa62ef4e34672ff6a792159d016
Marcus Westin <marcus.westin@gmail.com>
2013-08-09 00:09:46 -0400

    Add example InfiniteListViewController

---
+++ Examples/InfiniteListViewController.h
@@ -0,0 +1,12 @@
+//
+//  InfiniteListViewController.h
+//  Dogo-iOS
+//
+//  Created by Marcus Westin on 8/8/13.
+//  Copyright (c) 2013 Flutterby Labs Inc. All rights reserved.
+//
+
+#import "ListViewController.h"
+
+@interface InfiniteListViewController : ListViewController <ListViewDelegate>
+@end
