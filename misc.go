06a126106f40c61ec6c66dce85115505cd9a5149
Marcus Westin <marcus.westin@gmail.com>
2013-09-18 15:00:22 -0700

    misc functions

---
+++ misc.go
@@ -0,0 +1,44 @@
+package fun
+
+import (
+	cryptoRand "crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+	"math"
+	mathRand "math/rand"
+	"time"
+)
+
+func init() {
+	mathRand.Seed(time.Now().UnixNano())
+}
+
+// Returns a random int in the range [min, lessThan)
+func RandomBetween(min, lessThan int) int {
+	return mathRand.Intn(lessThan-min) + min
+}
+
+// Returns a random number with numDigits digits
+func RandomDigits(numDigits int) int {
+	min := int(math.Pow10(numDigits - 1))
+	lessThan := int(math.Pow10(numDigits))
+	return RandomBetween(min, lessThan)
+}
+
+func MakeUid(numChars int) (uid string, err error) {
+	if numChars%4 != 0 {
+		err = errors.New("uid length must be a multiple of 4")
+		return
+	}
+
+	buf := make([]byte, numChars)
+	_, err = io.ReadFull(cryptoRand.Reader, buf)
+	if err != nil {
+		return
+	}
+
+	uid = base64.URLEncoding.EncodeToString(buf)
+
+	return
+}
