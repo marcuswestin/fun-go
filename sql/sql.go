ee4543a077974fb2d5ba14cdbfeae4c3c607f213
Marcus Westin <marcus.westin@gmail.com>
2013-09-26 16:40:37 -0700

    Add sql connection pool

--- sql/sql.go
+++ sql/sql.go
@@ -2,29 +2,100 @@ package sql
 
 import (
 	"database/sql"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
-func SelectInt(conn *sql.DB, query string, args ...interface{}) (num int, found bool, err error) {
-	found, err = queryOne(conn, query, args, &num)
+func NewPool(sourceStrings []string) (pool *Pool, err error) {
+	queue := make(chan *sql.DB, len(sourceStrings))
+	var conn *sql.DB
+	for _, sourceStr := range sourceStrings {
+		conn, err = sql.Open("mysql", sourceStr)
+		if err != nil {
+			return
+		}
+		err = conn.Ping()
+		if err != nil {
+			return
+		}
+
+		queue <- conn
+	}
+	pool = &Pool{queue}
 	return
 }
 
-func SelectString(conn *sql.DB, query string, args ...interface{}) (str string, found bool, err error) {
-	found, err = queryOne(conn, query, args, &str)
+type Pool struct {
+	queue chan *sql.DB
+}
+
+// Query with fixed args
+func (p *Pool) Query(query string, args ...interface{}) (rows *sql.Rows, err error) {
+	conn := <-p.queue
+	defer func() { p.queue <- conn }()
+
+	FixArgs(args)
+	rows, err = conn.Query(query, args...)
+	if err != nil {
+		err = errors.New("sql.Query Error: " + err.Error() + ". Query: " + query + " Args: " + fmt.Sprint(args))
+	}
 	return
 }
 
-func SelectUInt(conn *sql.DB, query string, args ...interface{}) (num uint, found bool, err error) {
-	found, err = queryOne(conn, query, args, &num)
+// Execute with fixed args
+func (p *Pool) Exec(query string, args ...interface{}) (res sql.Result, err error) {
+	conn := <-p.queue
+	defer func() { p.queue <- conn }()
+
+	FixArgs(args)
+	res, err = conn.Exec(query, args...)
+	if err != nil {
+		err = errors.New("sql.Exec Error: " + err.Error() + ". Query: " + query + " Args: " + fmt.Sprint(args))
+	}
 	return
 }
 
-func queryOne(conn *sql.DB, query string, args []interface{}, out interface{}) (found bool, err error) {
-	rows, err := conn.Query(query, args...)
+/*
+Fix args by converting them to values of their underlying kind.
+This avoids problems in database/sql with e.g custom string types.
+Without FixArgs, the following code:
+
+	type Foo string
+	...
+	pool.Query("SELECT * WHERE Foo=?", Foo("bar"))
+
+would give you the error:
+
+	sql: converting Exec argument #1's type: unsupported type Foo, a string
+*/
+func FixArgs(args []interface{}) {
+	for i, arg := range args {
+		vArg := reflect.ValueOf(arg)
+		if vArg.Kind() == reflect.String {
+			args[i] = vArg.String()
+		}
+	}
+}
+
+func (p *Pool) SelectInt(query string, args ...interface{}) (num int, found bool, err error) {
+	found, err = p.queryOne(query, args, &num)
+	return
+}
+
+func (p *Pool) SelectString(query string, args ...interface{}) (str string, found bool, err error) {
+	found, err = p.queryOne(query, args, &str)
+	return
+}
+
+func (p *Pool) SelectUInt(query string, args ...interface{}) (num uint, found bool, err error) {
+	found, err = p.queryOne(query, args, &num)
+	return
+}
+
+func (p *Pool) queryOne(query string, args []interface{}, out interface{}) (found bool, err error) {
+	rows, err := p.Query(query, args...)
 	if err != nil {
 		return
 	}
@@ -45,30 +116,33 @@ func queryOne(conn *sql.DB, query string, args []interface{}, out interface{}) (
 	return
 }
 
-type TransactFunc func(conn *sql.DB) error
+// type TransactFunc func() error
 
-func Transact(conn *sql.DB, f TransactFunc) (err error) {
-	_, err = conn.Exec("START TRANSACTION")
-	if err != nil {
-		return
-	}
+// func (p *Pool) Transact(f TransactFunc) (err error) {
+// 	conn := <-p.queue
+// 	defer func() { p.queue <- conn }()
 
-	err = f(conn)
-	if err != nil {
-		_, rollbackError := conn.Exec("ROLLBACK")
-		if rollbackError != nil {
-			panic("Could not rollback transaction. TransactionFunc error: " + err.Error() + ". Rollback error:" + rollbackError.Error())
-		}
+// 	_, err = conn.Exec("START TRANSACTION")
+// 	if err != nil {
+// 		return
+// 	}
 
-	} else {
-		_, err = conn.Exec("COMMIT")
-	}
+// 	err = f(conn)
+// 	if err != nil {
+// 		_, rollbackError := conn.Exec("ROLLBACK")
+// 		if rollbackError != nil {
+// 			panic("Could not rollback transaction. TransactionFunc error: " + err.Error() + ". Rollback error:" + rollbackError.Error())
+// 		}
 
-	return
-}
+// 	} else {
+// 		_, err = conn.Exec("COMMIT")
+// 	}
 
-func UpdateOne(conn *sql.DB, query string, args ...interface{}) error {
-	res, err := conn.Exec(query, args...)
+// 	return
+// }
+
+func (p *Pool) UpdateOne(query string, args ...interface{}) error {
+	res, err := p.Exec(query, args...)
 	if err != nil {
 		return err
 	}
@@ -78,26 +152,27 @@ func UpdateOne(conn *sql.DB, query string, args ...interface{}) error {
 		return err
 	}
 	if affected != 1 {
-		return errors.New(fmt.Sprintf("UpdateOne affected %d rows", affected))
+		return errors.New(fmt.Sprintf("UpdateOne affected %d rows. Query: %q Args: %q", affected, query, args))
 	}
 
 	return nil
 }
 
-func InsertIgnoreId(conn *sql.DB, query string, args ...interface{}) (err error) {
-	_, err = Insert(conn, query, args...)
+func (p *Pool) InsertIgnoreId(query string, args ...interface{}) (err error) {
+	_, err = p.Insert(query, args...)
 	return
 }
 
-func Insert(conn *sql.DB, query string, args ...interface{}) (int64, error) {
-	res, err := conn.Exec(query, args...)
+func (p *Pool) Insert(query string, args ...interface{}) (id int64, err error) {
+	res, err := p.Exec(query, args...)
 	if err != nil {
-		return 0, err
+		return
 	}
-	return res.LastInsertId()
+	id, err = res.LastInsertId()
+	return
 }
 
-func Select(conn *sql.DB, output interface{}, sql string, args ...interface{}) error {
+func (p *Pool) Select(output interface{}, sql string, args ...interface{}) error {
 	// Check types
 	var outputPtr = reflect.ValueOf(output)
 	if outputPtr.Kind() != reflect.Ptr {
@@ -116,7 +191,7 @@ func Select(conn *sql.DB, output interface{}, sql string, args ...interface{}) e
 	}
 
 	// Query DB
-	var rows, err = conn.Query(sql, args...)
+	var rows, err = p.Query(sql, args...)
 	if err != nil {
 		return err
 	}
@@ -126,7 +201,8 @@ func Select(conn *sql.DB, output interface{}, sql string, args ...interface{}) e
 	if err != nil {
 		return err
 	}
-	structType := outputReflection.Type()
+
+	structType := outputReflection.Type().Elem()
 	for rows.Next() {
 		structPtrVal, err := structFromRow(structType, columns, rows)
 		if err != nil {
@@ -140,19 +216,22 @@ func Select(conn *sql.DB, output interface{}, sql string, args ...interface{}) e
 
 const selectOneTypeError = "fun/sql.SelectOne: expects a **struct, e.g var person *Person; c.SelectOne(&person, sql)"
 
-func SelectOne(conn *sql.DB, output interface{}, query string, args ...interface{}) error {
+func (p *Pool) SelectOne(output interface{}, query string, args ...interface{}) error {
 	// Check types
 	var outputReflectionPtr = reflect.ValueOf(output)
+	if !outputReflectionPtr.IsValid() {
+		panic(selectOneTypeError)
+	}
 	if outputReflectionPtr.Kind() != reflect.Ptr {
-		return errors.New(selectOneTypeError)
+		panic(selectOneTypeError)
 	}
 	var outputReflection = outputReflectionPtr.Elem()
 	if outputReflection.Kind() != reflect.Ptr {
-		return errors.New(selectOneTypeError)
+		panic(selectOneTypeError)
 	}
 
 	// Query DB
-	var rows, err = conn.Query(query, args...)
+	var rows, err = p.Query(query, args...)
 	if err != nil {
 		return err
 	}
@@ -163,8 +242,9 @@ func SelectOne(conn *sql.DB, output interface{}, query string, args ...interface
 		return err
 	}
 	if !rows.Next() {
-		return errors.New("fun/sql.SelectOne: got none")
+		return errors.New("fun/sql.SelectOne: got none. Query: " + query + " Args: " + fmt.Sprint(args))
 	}
+
 	structType := outputReflection.Type()
 	structPtrVal, err := structFromRow(structType, columns, rows)
 	if err != nil {
@@ -173,7 +253,7 @@ func SelectOne(conn *sql.DB, output interface{}, query string, args ...interface
 	outputReflection.Set(structPtrVal)
 
 	if rows.Next() {
-		return errors.New("fun/sql.SelectOne: got multiple rows")
+		return errors.New("fun/sql.SelectOne: got multiple rows. Query: " + query + " Args: " + fmt.Sprint(args))
 	}
 
 	return nil
@@ -181,6 +261,8 @@ func SelectOne(conn *sql.DB, output interface{}, query string, args ...interface
 
 func structFromRow(structType reflect.Type, columns []string, rows *sql.Rows) (structPtrVal reflect.Value, err error) {
 	structPtrVal = reflect.New(structType.Elem())
+
+	outputItemStructVal := structPtrVal.Elem()
 
 	vals := make([]interface{}, len(columns))
 	for i, _ := range columns {
@@ -192,9 +274,11 @@ func structFromRow(structType reflect.Type, columns []string, rows *sql.Rows) (s
 		return
 	}
 
-	outputItemStructVal := structPtrVal.Elem()
+	var uintVal uint64
+	var intVal int64
 	for i, column := range columns {
 		bytes := []byte(*vals[i].(*sql.RawBytes))
+		// bytes := []byte(*vals[i].(*[]byte))
 		if bytes == nil {
 			continue // Leave struct field empty
 		}
@@ -203,23 +287,22 @@ func structFromRow(structType reflect.Type, columns []string, rows *sql.Rows) (s
 		case reflect.String:
 			outputItemField.SetString(string(bytes))
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			val, num := binary.Uvarint(bytes) // Alt: strconv.ParseUint(string(bytes), 10, 64)
-			if num <= 0 {
-				err = errors.New("fun/sql: Could not parse uint from bytes")
+			uintVal, err = strconv.ParseUint(string(bytes), 10, 64)
+			if err != nil {
 				return
 			}
-			outputItemField.SetUint(reflect.ValueOf(val).Uint())
+			outputItemField.SetUint(reflect.ValueOf(uintVal).Uint())
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			val, num := binary.Varint(bytes) // Alt: strconv.ParseInt(string(bytes), 10, 64)
-			if num <= 0 {
-				err = errors.New("fun/sql: Could not parse uint from bytes")
+			intVal, err = strconv.ParseInt(string(bytes), 10, 64)
+			if err != nil {
 				return
 			}
-			outputItemField.SetInt(reflect.ValueOf(val).Int())
+			outputItemField.SetInt(reflect.ValueOf(intVal).Int())
 		default:
 			err = errors.New("fun/sql: Bad row value for column: " + column)
 			return
 		}
 	}
+
 	return
 }
