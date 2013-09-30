facbdb4a7f0a3e57644ccb04d9e480c6ea96ab87
Marcus Westin <marcus.westin@gmail.com>
2013-09-30 13:26:50 -0700

    Allow for Each func to take i argument

--- async/Each.go
+++ async/Each.go
@@ -18,8 +18,9 @@ func Each(items interface{}, fn interface{}) (err error) {
 
 	vFn := reflect.ValueOf(fn)
 	tFn := vFn.Type()
-	if tFn.NumIn() != 1 {
-		return expects("fn to take an argument")
+	numIn := tFn.NumIn()
+	if numIn != 1 && numIn != 2 {
+		return expects("fn to take one or two arguments")
 	}
 	if tFn.In(0) != tItems.Elem() {
 		return expects("fn argument type to match items")
@@ -31,7 +32,13 @@ func Each(items interface{}, fn interface{}) (err error) {
 	errChan := make(chan error)
 
 	for i := 0; i < vItems.Len(); i++ {
-		args := []reflect.Value{vItems.Index(i)}
+		var args []reflect.Value
+		if numIn == 1 {
+			args = []reflect.Value{vItems.Index(i)}
+		} else {
+			args = []reflect.Value{vItems.Index(i), reflect.ValueOf(i)}
+		}
+
 		go func() {
 			vErr := vFn.Call(args)
 			if vErr[0].IsNil() {
