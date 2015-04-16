41463b6d6dc8669de1d3966d6cac94ebdd66c913
Marcus Westin <narcvs@gmail.com>
2015-04-16 12:49:23 -0400

    Correctly detect duplicate table error

--- sql/Shard.go
+++ sql/Shard.go
@@ -76,7 +76,8 @@ func (s *shardConn) Exec(query string, args ...interface{}) (res sql.Result, err
 }
 func IsDuplicateExecError(err error) bool {
 	return strings.HasPrefix(err.Error(), "sql.Exec Error: Error 1060: Duplicate column name") ||
-		strings.HasPrefix(err.Error(), "sql.Exec Error: Error 1061: Duplicate key name")
+		strings.HasPrefix(err.Error(), "sql.Exec Error: Error 1061: Duplicate key name") ||
+		strings.HasPrefix(err.Error(), "sql.Exec Error: Error 1050: Table")
 }
 func (s *shardConn) ExecIgnoreDuplicateError(query string, args ...interface{}) (res sql.Result, err error) {
 	res, err = s.Exec(query, args...)
