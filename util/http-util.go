4ffb672116290f014f91d970a0bf859dedd04ca4
Marcus Westin <narcvs@gmail.com>
2015-07-28 19:30:53 -0400

    Make HTTP request helpers safe by always closing the body

--- util/http-util.go
+++ util/http-util.go
@@ -1,37 +1,46 @@
 package util
 
 import (
+	"io/ioutil"
 	"net/http"
 	"strings"
 
 	"github.com/marcuswestin/fun-go/errs"
 )
 
-func HTTPGet(url string) (*http.Response, errs.Err) {
+func HTTPGet(url string) (statusCode int, body string, err errs.Err) {
 	res, stdErr := http.Get(url)
-	if stdErr != nil {
-		return nil, errs.Wrap(stdErr, errs.Info{"URL": url})
-	}
-	return res, nil
+	return wrapHandleRes(url, res, stdErr)
 }
 
-func HTTPPostJSON(url string, jsonPayload interface{}) (*http.Response, errs.Err) {
+func HTTPPostJSON(url string, jsonPayload interface{}) (statusCode int, body string, err errs.Err) {
 	jsonReader, err := JSONReader(jsonPayload)
 	if err != nil {
-		return nil, err
+		return
 	}
 	res, stdErr := http.Post(url, "application/json", jsonReader)
-	if stdErr != nil {
-		return nil, errs.Wrap(stdErr, errs.Info{"URL": url, "Payload": jsonPayload})
-	}
-	return res, nil
+	return wrapHandleRes(url, res, stdErr)
 }
 
-func HTTPPostString(url string, str string) (*http.Response, errs.Err) {
+func HTTPPostString(url string, str string) (statusCode int, body string, err errs.Err) {
 	reader := strings.NewReader(str)
 	res, stdErr := http.Post(url, "text/plain", reader)
+	return wrapHandleRes(url, res, stdErr)
+}
+
+func wrapHandleRes(url string, res *http.Response, stdErr error) (statusCode int, body string, err errs.Err) {
 	if stdErr != nil {
-		return nil, errs.Wrap(stdErr, errs.Info{"URL": url, "String": str})
+		err = errs.Wrap(stdErr, errs.Info{"URL": url})
+		return
 	}
-	return res, nil
+	defer res.Body.Close()
+
+	statusCode = res.StatusCode
+	bodyBytes, stdErr := ioutil.ReadAll(res.Body)
+	if stdErr != nil {
+		err = errs.Wrap(stdErr, errs.Info{"URL": url})
+		return
+	}
+	body = string(bodyBytes)
+	return
 }
